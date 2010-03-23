97b213b785f2e2572c2de0cefabc7b0fcd648abb
isaacwedin <isaacwedin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-23 15:42:37 +0000

    more conflict insulation, tag list now starts with popular tags, tagging 1.8.1 release
    git-svn-id: https://plugins.svn.wordpress.org/flickpress/tags/1.8.1@220955 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickpress.php
+++ flickpress.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: flickpress
 Plugin URI: http://familypress.net/flickpress/
-Description: A multi-user Flickr tool plus widget. Creates database tables to store Flickr ids and cache data. Last tested and working with WordPress 2.9.1. Uses Dan Coulter's excellent phpFlickr class. Requires a Flickr API key.
-Version: 1.6
+Description: A multi-user Flickr tool plus widget. Creates database tables to store Flickr ids and cache data. Uses Dan Coulter's excellent phpFlickr class. Requires a Flickr API key.
+Version: 1.8.1
 Author: Isaac Wedin
 Author URI: http://familypress.net/
 */
@@ -57,7 +57,7 @@ function flickpress_popup_javascript() {
 <script type="text/javascript">
 //<![CDATA[
 function edflickpress() {
-   tb_show("' . __('flickpress: insert Flickr photos','flickpress') . '","' . get_bloginfo('wpurl') . '/wp-content/plugins/flickpress/popup.php?action=users&amp;TB_iframe=true",false);
+   tb_show("' . __('flickpress: insert Flickr photos','flickpress') . '","' . get_bloginfo('wpurl') . '/wp-content/plugins/flickpress/popup.php?fpaction=users&amp;TB_iframe=true",false);
 }
 //]]>
 </script>
@@ -94,7 +94,7 @@ function flickpress_options_subpanel() {
 	}
 	if (!empty($flickpress_options['apikey'])) {
 		if (!flickpress_check_key($flickpress_options['apikey'])) {
-			echo "\n<div id='flickpress-warning' class='updated fade'><p><strong>Error:</strong> Your Flickr API key seems to be invalid, please verify it is correct.</p></div>\n";
+			echo "\n<div id='flickpress-warning' class='updated fade'><p><strong>Error:</strong> Your Flickr API key seems to be invalid, please verify it is correct. This can also mean the Flickr API itself has changed, so if your key is correct please check for a plugin update.</p></div>\n";
 		}
 	}
 	if (!current_user_can($flickpress_options['usecap'])) { // they're an admin, so the capability *must* be wrong...
@@ -175,7 +175,7 @@ function flickpress_management() {
 		  echo '
 		  <div class="wrap">
 		  <h2>' . __('flickpress manager','flickpress') . '</h2>
-		  <p>' . __('You can manually manage flickpress users here. You should probably only delete users...you can also add users but that is much easier to do from the popup tool because you can look up users by email address there.','flickpress') . '</p>
+		  <p>' . __('You can manually manage flickpress users here. It is possible to add users here but that is much easier to do from the popup tool because you can look up users by email address there.','flickpress') . '</p>
 		  <form name="flickpress" method="post">
 		  <p class="submit"><input type="submit" name="Submit" value="' . __('Update','flickpress') . ' &raquo;" /></p>
 		  <input type="hidden" name="flickpress_update" value="update" />
@@ -247,8 +247,15 @@ function flickpress_delete($id) {
 		  }
 }
 
-// run the table installer when the plugin is activated
+function flickpress_uninstall() {
+	global $wpdb;
+	$cache_table = $wpdb->prefix . 'flickpress_cache';
+	$wpdb->query("DROP TABLE IF EXISTS $cache_table");
+}
+
+// activation and deactivation hooks
 register_activation_hook( __FILE__, 'flickpress_table_install');
+register_deactivation_hook( __FILE__, 'flickpress_uninstall');
 
 // a simple template function to display photos in a sidebar or somesuch
 function flickpress_photos($email,$numphotos=3,$before='',$after='<br />',$fpclass='centered') {
@@ -258,8 +265,8 @@ function flickpress_photos($email,$numphotos=3,$before='',$after='<br />',$fpcla
 		$flick = new phpFlickpress($flickpress_options['apikey']);
 		$fcon = "mysql://" . DB_USER . ":" . DB_PASSWORD . "@" . DB_HOST . "/" . DB_NAME;
 		$flick->enableCache($type = 'db', $fcon , $cache_expire = 600, $table = $table_prefix.'flickpress_cache');
-		$check = $flick->test_echo();
-		if ($check['stat'] == 'ok') {
+		$check = $flick->photos_getRecent(NULL,1,1);
+		if ($check['page'] == 1) {
 			$user_id = $flick->people_findByEmail($email);
 			$user_info = $flick->people_getInfo($user_id['id']);
 			$photos_url = $user_info['photosurl'];
